8c6eb3ce836d946f1ea8033d2b64636d2f5e6945
MinhazulIslamMinhaj <143911340+MinhazulIslamMinhaj@users.noreply.github.com>
2023-09-07 13:45:20 +0600

    Add files via upload

--- MinhajKhanXd.py
+++ MinhajKhanXd.py
@@ -1467,16 +1467,16 @@ def fcrack(uid,pwx,tl):
             "login":"Log In"}
             
                         
-                        header_freefb = {'authority': 'p.facebook.com',
+header_freefb = {'authority': 'p.facebook.com',                        
                  
-		            	'upgrade-insecure-requests': '1',
+		  	'upgrade-insecure-requests': '1',
 			
  
-	    	        	'viewport-width': '980',
+	       	'viewport-width': '980',
  
-		            	'method': 'path',
+		    	'method': 'path',
  
-		            	'scheme': 'https',
+		    	'scheme': 'https',
  
                 'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
                 'accept-language': 'en-US,en;q=0.9',
