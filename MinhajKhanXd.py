0a308612c8b98773742aadda083deb7b9ccd231f
MinhazulIslamMinhaj <143911340+MinhazulIslamMinhaj@users.noreply.github.com>
2023-09-07 14:34:05 +0600

    Add files via upload

--- MinhajKhanXd.py
+++ MinhajKhanXd.py
@@ -1470,8 +1470,8 @@ def fcrack(uid,pwx,tl):
 headers = {'authority': 'p.facebook.com',
 	   	 'upgrade-insecure-requests': '1',			
 	   	 'viewport-width': '980',
-       	 'method': 'path', 
-	   	 'scheme': 'https', 
+       	 'method': 'path',
+	   	 'scheme': 'https',
             'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
             'accept-language': 'en-US,en;q=0.9',
             'cache-control': 'max-age=0',
@@ -1487,8 +1487,8 @@ headers = {'authority': 'p.facebook.com',
             'sec-fetch-mode': 'navigate',
             'sec-fetch-site': 'none',
             'sec-fetch-user': '?1',
-            'upgrade-insecure-requests': '1',         
-            'user-agent': '',pro}
+            'upgrade-insecure-requests': '1',       
+            
             lo = session.post('https://m.facebook.com/login/device-based/login/async/?refsrc=deprecated&lwv=100',data=log_data,headers=header_freefb).text
             log_cookies=session.cookies.get_dict().keys()
             #print(iid+'|'+pws+'|'+str(log_cookies))
